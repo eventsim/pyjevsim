9b9334fabb762a4762ff88bbd7921133baaf48ba
Jaiyun Lee <jaiyun16@gmail.com>
2023-09-26 14:12:47 +0900

    initial upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pyevsim",
+    version="1.1.6",
+    license='MIT',
+    author="Changbeom Choi",
+    author_email="me@cbchoi.info",
+    description="A library that provides a Modeling & Simulation Environment for Discrete Event System Formalism",
+    long_description=open('README.md').read(),
+    url="https://github.com/eventsim/pyevsim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
