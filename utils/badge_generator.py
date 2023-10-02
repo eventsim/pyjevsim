9c3f2872f8bad2d838e8ef70dcb2d482d168d586
Changbeom Choi <cbchoi@users.noreply.github.com>
2023-10-03 04:08:12 +0900

    Update badge_generator.py

--- utils/badge_generator.py
+++ utils/badge_generator.py
@@ -4,7 +4,8 @@ import string
 score = 0
 with subprocess.Popen(["pylint","pyjevsim"], stdout=subprocess.PIPE) as proc:
     tokens = str(proc.stdout.read()).split(':')
-    score = tokens[-1].split(',')[0][1:-3]
-    print(f"pylint score:{score}")
+    score = tokens[-1].split(',')[0][1:-8].split()
+    
+    print(f"pylint score:{score[-1]}")
 
 subprocess.Popen(["anybadge", f"--value={score}", "-o", "--file=utils/pylint.svg", "pylint"], stdout=subprocess.PIPE)
